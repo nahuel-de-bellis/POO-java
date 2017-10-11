5fed671e4a55827e0072935fd955ff0ff8647616
nahuel de bellis <debellisnahuel@gmail.com>
2017-10-10 21:02:35 -0300

    Create Main.java

---
+++ leerTXT/Main.java
@@ -0,0 +1,14 @@
+package leerTXT;
+
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args){
+		Leer ar = new Leer();
+		ar.leer("Un.txt");
+		ar.escribir("Un.txt");
+		
+	}
+
+}
